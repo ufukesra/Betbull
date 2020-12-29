d64fbe5d895594bedaeeb75d6b526bfb601d3ff4
ufuk58 <ufuksahinduran.uk@gmail.com>
2020-12-29 00:59:35 +0000

    Runner class created and all the Cucumber features defined

---
+++ src/test/java/pages/CukesRunner.java
@@ -0,0 +1,19 @@
+package pages;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"json:target/cucumber.json",
+                "html:target/default-cucumber-reports"},
+        features = {"src/test/resources/features"},
+        glue = {"step_definitions"},
+        //tags = {"@api","@mobile","web"},
+        dryRun = false
+)
+public class CukesRunner {
+
+
+}
